cc54e6747aba2f0f3d652394836f7aeba5fb5ec1
OO <ooauroras@gmail.com>
2020-04-13 04:06:48 +0800

    style: use the proper method name siftdown.

--- array-sort/heap-sort.js
+++ array-sort/heap-sort.js
@@ -1,4 +1,4 @@
-function sink(arr, start, end) {
+function siftDown(arr, start, end) {
     let rootIndex = start,
         swapIndex = start,
         leftChildIndex = rootIndex * 2 + 1,
@@ -25,7 +25,7 @@ function sink(arr, start, end) {
 
 function minHeapify(arr) {
     for (let i = Math.floor((arr.length - 2) / 2); i >= 0; i--) {
-        sink(arr, i, arr.length - 1);
+        siftDown(arr, i, arr.length - 1);
     }
 }
 
@@ -36,7 +36,7 @@ function heapSort(arr) {
     while (end > 0) {
         [arr[0], arr[end]] = [arr[end], arr[0]];
         end--;
-        sink(arr, 0, end);
+        siftDown(arr, 0, end);
     }
 
     return arr;
