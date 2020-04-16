98c95d02dacaa1ff2542d9265a54ce8078874a23
OO <ooauroras@gmail.com>
2020-04-16 16:19:04 +0800

    fix: fix shellSort bug.

--- array-sort/shell-sort.js
+++ array-sort/shell-sort.js
@@ -1,7 +1,7 @@
 function shellSort(arr) {
     let temp, i, j;
     for (let step = arr.length >> 1; step >= 1; step >>= 1) {
-        for (i = step; i < arr.length; i += step) {
+        for (i = step; i < arr.length; i++) {
             temp = arr[i];
             for (j = i - step; j >= 0 && temp < arr[j]; j -= step) {
                 arr[j + step] = arr[j];
